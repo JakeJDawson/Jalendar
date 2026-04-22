acfa4afbc1d095f841b34851cd7d9eab5ee70d2d
JakeJDawson <EagleScoutJJD@Gmail.com>
2026-04-22 11:17:22 -0400

    Created basic Maven Spring Boot project. Currently, it only opens to show "Hello World", as I've never used Maven or Spring Boot before, but this felt like a good starting point.

---
+++ jalendar-service/src/main/java/com/jalendar/jalendar_service/JalendarController.java
@@ -0,0 +1,13 @@
+package com.jalendar.jalendar_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JalendarController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+}
---
+++ jalendar-service/src/main/java/com/jalendar/jalendar_service/JalendarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jalendar.jalendar_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JalendarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JalendarServiceApplication.class, args);
+	}
+
+}
